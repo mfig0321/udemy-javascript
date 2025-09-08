f52a03220697490f4495f166df5561923ccb1ba0
Michael Figueroa <michael.figueroa73@gmail.com>
2025-09-08 18:02:03 -0400

    Section 8 Data type assignments - refactored assignments into own file.

---
+++ Section2/starter/assigments/assignments.js
@@ -0,0 +1,18 @@
+// Section 2: Assignments - Variables and Data Types
+let country = 'USA';
+let continent = 'North America';
+let population = 331; // in millions
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// Data types demonstration
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
--- Section2/starter/script.js
+++ Section2/starter/script.js
@@ -1,5 +1,6 @@
+/*
 let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
+// console.log(40 + 8 + 23 - 10);
 
 console.log('Jonas');
 console.log(23);
@@ -10,6 +11,7 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
+// Variable naming conventions
 let jonas_matilda = 'JM';
 let $function = 27;
 
@@ -23,3 +25,24 @@ let job1 = 'programmer';
 let job2 = 'teacher';
 
 console.log(myFirstJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null); // this is a bug in JavaScript, it should return 'object' but it returns 'null'
