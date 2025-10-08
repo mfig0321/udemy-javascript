d50d70f6c1e8809b319480e3995a004d5e7f9d7d
Michael Figueroa <michael.figueroa73@gmail.com>
2025-10-08 15:49:52 -0400

    Ternary operator

--- Section2/starter/script.js
+++ Section2/starter/script.js
@@ -248,7 +248,7 @@ if(hasDriversLicense && hasGoodVision && !isTired) {
 } else {
     console.log('Someone else should drive...');
 }
--*/
+-
 
 // The Switch Statement
 const day = 'monday';
@@ -274,3 +274,24 @@ switch(day){
     default:
         console.log('Not a valid day!');    
 }
+*/
+// The conditional (Ternary) Operator
+
+const age = 23;
+
+age >= 18 ? console.log('I like to drink wine') : 
+console.log('I like to drink water');
+
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink);
+
+let drink2;
+if(age >= 18) {
+    drink2 = 'wine';
+} else {
+    drink2 = 'water';
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
+
