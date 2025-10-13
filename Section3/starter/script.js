a61c3b9abf8210f593d2299a2a408884ca489540
Michael Figueroa <michael.figueroa73@gmail.com>
2025-10-13 16:21:44 -0400

    Functions Intro

--- Section3/starter/script.js
+++ Section3/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -8,4 +8,28 @@ if(hasDriversLicense) console.log('I can drive :D');
 
 // const interface = 'Audio'; // reserved word
 // const private = 534; // reserved word
+*/
+
+// Functions
+function logger() {
+    console.log('My name is Jonas');
+}
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
+console.log(num);
 
