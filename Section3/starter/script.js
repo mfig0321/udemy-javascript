1803e213667e55dbb5004bd52f2e709484764ed1
Michael Figueroa <michael.figueroa73@gmail.com>
2025-10-13 17:31:12 -0400

    Review Functions

--- Section3/starter/script.js
+++ Section3/starter/script.js
@@ -65,7 +65,7 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 
 console.log(yearsUntilRetirement(1991,'Jonas'));
 console.log(yearsUntilRetirement(1980, 'Bob'));
-*/
+
 
 function cutFruitPieces(fruit) {
     return fruit * 4;
@@ -80,3 +80,25 @@ function fruitProcessor(apples, oranges) {
 }
 
 console.log(fruitProcessor(2, 3));
+*/
+
+// Reviewing Functions
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years.`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired.`);
+        return -1;
+    }
+}
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1970, 'Mike'));
