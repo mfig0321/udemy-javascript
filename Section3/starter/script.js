b00971eac97e25df3cc0cc49e95451e472a98caa
Michael Figueroa <michael.figueroa73@gmail.com>
2025-10-13 17:21:07 -0400

    Functtions Calling Other Functions

--- Section3/starter/script.js
+++ Section3/starter/script.js
@@ -49,7 +49,7 @@ const calcAge2 = function (birthYear) {
     return 2037 - birthYear;
 }
 
-*/
+
 // Arrow Functions
 
 const calcAge3 = birthYear => 2037 - birthYear;
@@ -65,3 +65,18 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 
 console.log(yearsUntilRetirement(1991,'Jonas'));
 console.log(yearsUntilRetirement(1980, 'Bob'));
+*/
+
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} apple pieces and ${orangePieces} orange pieces.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
